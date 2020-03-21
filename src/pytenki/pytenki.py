a994be63725bdf6f3a6468bd8b60bee5d015faf6
Gary Sentosa <gary.sentosa@gmail.com>
2020-03-21 10:35:35 +0900

    Move try except block to decorator for DRY code

--- src/pytenki/pytenki.py
+++ src/pytenki/pytenki.py
@@ -1,6 +1,15 @@
 from gpiozero import LEDBoard
 
 
+def _exc_attr_err(func):
+    def wrapper(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except AttributeError:
+            pass
+    return wrapper
+
+
 class PyTenki:
     def __init__(self, forecast=None, led_pins=None):
         self._forecast = forecast
@@ -30,82 +39,70 @@ class PyTenki:
         except (AttributeError, TypeError):
             pass
 
+    @_exc_attr_err
     def assign_leds(self, led_pins):
-        try:
-            self._close_leds()
+        self._close_leds()
 
-            self._leds = LEDBoard(
-                fine=led_pins.get('fine'),
-                cloud=led_pins.get('cloud'),
-                rain=led_pins.get('rain'),
-                snow=led_pins.get('snow'),
-                pwm=True,
-            )
-        except AttributeError:
-            pass
+        self._leds = LEDBoard(
+            fine=led_pins.get('fine'),
+            cloud=led_pins.get('cloud'),
+            rain=led_pins.get('rain'),
+            snow=led_pins.get('snow'),
+            pwm=True,
+        )
 
+    @_exc_attr_err
     def _close_leds(self):
-        try:
-            self._leds.close()
-        except AttributeError:
-            pass
+        self._leds.close()
 
+    @_exc_attr_err
     def _operate_fine_led(self):
-        try:
-            weather = self.forecast.get('weather')
+        weather = self.forecast.get('weather')
 
-            if weather.startswith('晴'):
-                self._leds.fine.on()
-            elif 'のち晴' in weather:
-                self._leds.fine.pulse()
-            elif '時々晴' in weather:
-                self._leds.fine.blink()
-            else:
-                self._leds.fine.off()
-        except AttributeError:
-            pass
+        if weather.startswith('晴'):
+            self._leds.fine.on()
+        elif 'のち晴' in weather:
+            self._leds.fine.pulse()
+        elif '時々晴' in weather:
+            self._leds.fine.blink()
+        else:
+            self._leds.fine.off()
 
+    @_exc_attr_err
     def _operate_cloud_led(self):
-        try:
-            weather = self.forecast.get('weather')
+        weather = self.forecast.get('weather')
 
-            if weather.startswith('曇'):
-                self._leds.cloud.on()
-            elif 'のち曇' in weather:
-                self._leds.cloud.pulse()
-            elif '時々曇' in weather:
-                self._leds.cloud.blink()
-            else:
-                self._leds.cloud.off()
-        except AttributeError:
-            pass
+        if weather.startswith('曇'):
+            self._leds.cloud.on()
+        elif 'のち曇' in weather:
+            self._leds.cloud.pulse()
+        elif '時々曇' in weather:
+            self._leds.cloud.blink()
+        else:
+            self._leds.cloud.off()
 
+    @_exc_attr_err
     def _operate_rain_led(self):
-        try:
-            weather = self.forecast.get('weather')
+        weather = self.forecast.get('weather')
 
-            if weather.startswith('雨'):
-                self._leds.rain.on()
-            elif 'のち雨' in weather:
-                self._leds.rain.pulse()
-            elif '時々雨' in weather:
-                self._leds.rain.blink()
-            else:
-                self._leds.rain.off()
-        except AttributeError:
-            pass
+        if weather.startswith('雨'):
+            self._leds.rain.on()
+        elif 'のち雨' in weather:
+            self._leds.rain.pulse()
+        elif '時々雨' in weather:
+            self._leds.rain.blink()
+        else:
+            self._leds.rain.off()
 
+    @_exc_attr_err
     def _operate_snow_led(self):
-        try:
-            weather = self.forecast.get('weather')
+        weather = self.forecast.get('weather')
 
-            if weather.startswith('雪'):
-                self._leds.snow.on()
-            elif 'のち雪' in weather:
-                self._leds.snow.pulse()
-            elif '時々雪' in weather:
-                self._leds.snow.blink()
-            else:
-                self._leds.snow.off()
-        except AttributeError:
-            pass
+        if weather.startswith('雪'):
+            self._leds.snow.on()
+        elif 'のち雪' in weather:
+            self._leds.snow.pulse()
+        elif '時々雪' in weather:
+            self._leds.snow.blink()
+        else:
+            self._leds.snow.off()
